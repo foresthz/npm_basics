90859c8e2e9572c37d71a490a95351eb24d1913f
38 <host@host.com>
2018-10-20 19:53:12 +0800

    add require lab

--- lab/simple_cli/bin/simple_cli.js
+++ lab/simple_cli/bin/simple_cli.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
 
 console.info('from simple_cli bin ..');
+
+let test = require('../src/test');
+console.info('test: ', test);
---
+++ lab/simple_cli/src/test.js
@@ -0,0 +1,8 @@
+
+class TestManager {
+
+}
+
+module.exports = {
+    TestManager
+}
