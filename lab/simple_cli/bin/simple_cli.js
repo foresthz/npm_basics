0e7dd302d49835e4ef30c42c0fe72322116654f0
38 <host@host.com>
2018-10-20 20:49:44 +0800

    add debug mechanism

--- lab/simple_cli/bin/simple_cli.js
+++ lab/simple_cli/bin/simple_cli.js
@@ -4,3 +4,9 @@ console.info('from simple_cli bin ..');
 
 let test = require('../src/test');
 console.info('test: ', test);
+
+const {TestManager} = require('../src/test')
+
+debugger;
+
+console.info('after break point simple cli ..');
